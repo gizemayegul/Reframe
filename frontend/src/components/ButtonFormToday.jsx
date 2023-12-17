75441b49fdd994d8c9ab614d5c147a080b2e483f
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-12-17 17:33:45 +0100

    arrow-right src

--- frontend/src/components/ButtonFormToday.jsx
+++ frontend/src/components/ButtonFormToday.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import "./ButtonForm.css";
-
+import ArrowRight from "../assets/arrow-right.svg";
 export default function ButtonForm({ label, onClick, size }) {
   return (
     <Button className="btn-form" size={size} onClick={onClick}>
       {label}
-      <img src="../../public/arrow-right.svg" alt="arrow_right" />
+      <img src={ArrowRight} alt="arrow_right" />
     </Button>
   );
 }
