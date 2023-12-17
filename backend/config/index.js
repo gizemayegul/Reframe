20a43ca05eca815ce4b47d9c15db673046828dd5
Gizem <109289446+gizemayegul@users.noreply.github.com>
2023-12-17 14:59:29 +0100

    cors empty

--- backend/config/index.js
+++ backend/config/index.js
@@ -22,11 +22,7 @@ module.exports = (app) => {
   app.set("trust proxy", 1);
 
   // controls a very specific header to pass headers from the frontend
-  app.use(
-    cors({
-      origin: [FRONTEND_URL],
-    })
-  );
+  app.use(cors());
 
   // In development environment the app logs
   app.use(logger("dev"));
